ed11215518f25f2489b231d2564cbea70f633b03
callumspeirs <callum_speirs@protonmail.com>
2021-11-19 11:51:21 +0000

    Update app to include form submission and mongodb integration

--- app.js
+++ app.js
@@ -2,14 +2,61 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+const router = express.Router();
 
-app.use(express.json());
-app.use(express.static("express"));
+const { MongoClient }  = require("mongodb");
+const url = "mongodb://localhost:27017/";
+const client = new MongoClient(url);
 
-// default URL for website
-app.use('/', function(req,res){
-  res.sendFile(path.join(__dirname+'/pages/index.html'));
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// Page call backs
+router.get('/', (req, res) =>
+{
+  res.sendFile(path.join(__dirname + '/pages/index.html'));
 });
+
+router.get('/about', (req, res) =>
+{
+  res.sendFile(path.join(__dirname + '/pages/about.html'));
+});
+
+router.get('/form', (req, res) =>
+{
+  res.sendFile(path.join(__dirname + '/pages/form.html'));
+});
+
+// Form submit
+router.post('/submit', async (req, res) => {
+  // Create mongodb connection
+  try
+  {
+    await client.connect();
+    const database = client.db("publictransport");
+    const collection = database.collection("interest");
+
+    // Insert submission into database
+    await collection.insertOne(req.body);
+
+    const submissions = await collection.find().toArray();
+    console.log(JSON.stringify(submissions));
+  }
+  catch (e)
+  {
+    console.log(e)
+  }
+  finally
+  {
+    await client.close();
+  }
+
+  // Send user back to form page
+  res.sendFile(path.join(__dirname + '/pages/form.html'));
+});
+
+app.use('/', router);
 
 const server = http.createServer(app);
 const port = 3000;
